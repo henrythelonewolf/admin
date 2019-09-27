91303a1f4b6daed5260a03f3620075c5141aa896
alkyu92 <alkyu92@gmail.com>
2019-09-27 18:24:00 +0800

    updates

--- src/layouts/admins/components/calendars/CalendarIndex.js
+++ src/layouts/admins/components/calendars/CalendarIndex.js
@@ -4,24 +4,86 @@ import { Container } from 'semantic-ui-react';
 import {
   Calendar,
   momentLocalizer,
+  Views,
 } from 'react-big-calendar';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+
 import moment from 'moment';
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+
+const localizer = momentLocalizer(moment);
+const DnDCalendar = withDragAndDrop(Calendar)
 
 export default class CalendarIndex extends React.Component {
-  state = {
-    events: []
+  constructor(props) {
+    super(props)
+    this.state = {
+      events: [
+        {
+          start: new Date(),
+          end: new Date(moment()),
+          title: "Some title"
+        }
+      ],
+    }
+
+    this.onEventDrop = this.onEventDrop.bind(this)
+    this.onEventResize = this.onEventResize.bind(this)
+    this.newEvent = this.newEvent.bind(this)
   }
+
+  onEventDrop({ event, start, end, isAllDay: droppedOnAllDaySlot }) {
+    const { events } = this.state;
+
+    const idx = events.indexOf(event);
+    let allDay = event.allDay;
+
+    if (!event.allDay && droppedOnAllDaySlot) {
+      allDay = true
+    } else if (event.allDay && !droppedOnAllDaySlot) {
+      allDay = false
+    }
+
+    const updatedEvent = { ...event, start, end, allDay };
+
+    const nextEvents = [ ...events ];
+    nextEvents.splice(idx, 1, updatedEvent);
+
+    this.setState({ events: nextEvents });
+  };
+
+  onEventResize({ event, start, end }) {
+    const { events } = this.state;
+
+    const nextEvents = events.map(existingEvent => {
+      return existingEvent.id == event.id ? { ...existingEvent, start, end } : existingEvent
+    })
+
+    this.setState({ events: nextEvents });
+  }
+
+  newEvent(event) {
+
+  }
+
   render(){
     const { events } = this.state;
 
     return (
       <Container>
-        <Calendar
-          localizer={momentLocalizer(moment)}
+        <DnDCalendar
+          selectable
+          resizable
+          localizer={localizer}
           events={events}
-          startAccessor={'start'}
-          endAccessor={'end'}
+          onEventDrop={this.onEventDrop}
+          onEventResize={this.onEventResize}
+          onSelectSlot={this.newEvent}
+          onDragStart={console.log}
+          defaultView={Views.MONTH}
+          defaultDate={new Date()}
           style={{ height: '70vh' }}
         />
       </Container>
