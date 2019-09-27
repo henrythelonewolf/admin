8428137d58097f6e0fb9022227f9c8ee723cbc87
alkyu92 <alkyu92@gmail.com>
2019-09-28 03:42:48 +0800

    added react data grid

--- src/layouts/admins/components/orders/OrdersIndex.js
+++ src/layouts/admins/components/orders/OrdersIndex.js
@@ -1,10 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Tab, Button } from 'semantic-ui-react';
+
+import ReactDataGrid from 'react-data-grid';
+import { Toolbar, Data, Filters } from 'react-data-grid-addons';
 
 import PendingOrders from './PendingOrders';
 import ProcessedOrders from './ProcessedOrders';
 import OutForDeliveryOrders from './OutForDeliveryOrders';
 import OnHoldOrders from './OnHoldOrders';
+
+const defaultColumnProperties = {
+  filterable: true,
+  width: 160,
+}
+
+const selectors = Data.Selectors;
+
+const {
+  NumericFilter,
+  AutoCompleteFilter,
+  MultiSelectFilter,
+  SingleSelectFilter,
+} = Filters;
+
+const rows = [
+  { id: 1, title: 'First Row', description: 'This is first row' },
+  { id: 2, title: 'Second Row', description: 'This is second row' }
+]
+
+const columns = [
+  {
+    key: 'id',
+    name: 'ID',
+    filterRenderer: NumericFilter
+  },
+  {
+    key: 'title',
+    name: 'Title',
+    filterRenderer: MultiSelectFilter,
+  },
+  {
+    key: 'description',
+    name: 'Description',
+  },
+].map( col => ({ ...col, ...defaultColumnProperties }));
+
+const handleFilterChange = filter => filters => {
+  const newFilters = { ...filters };
+
+  if (filter.filterTerm) {
+    newFilters[filter.column.key] = filter;
+  } else {
+    delete newFilters[filter.column.key];
+  }
+
+  return newFilters;
+}
+
+function getValidFilterValues(rows, columnId){
+  return rows.map( row => row[columnId]).filter( (item, i, a) => {
+    return i === a.indexOf(item);
+  })
+}
+
+function getRows(rows, filters){
+  return selectors.getRows({ rows, filters });
+}
+
+function MyDataGrid({ rows }) {
+  const [filters, setFilters] = useState({});
+  const filteredRows = getRows(rows, filters);
+  return (
+    <ReactDataGrid
+      rowGetter={ i => filteredRows[i] }
+      columns={columns}
+      rowsCount={filteredRows.length}
+      minHeight={500}
+      toolbar={<Toolbar enableFilter={true} />}
+      onAddFilter={ filter => setFilters(handleFilterChange(filter))}
+      onClearFilters={ () => setFilters({})}
+      getValidFilterValues={ columnKey => getValidFilterValues(rows, columnKey)}
+    />
+  )
+}
 
 export default class BoilerPlate extends React.Component {
   mainPanes = [
@@ -33,9 +111,12 @@ export default class BoilerPlate extends React.Component {
       )}
 
       {toggleView && (
+        <>
         <Button onClick={ () => this.setState({ toggleView: !toggleView})}>
         Individual View
         </Button>
+        <MyDataGrid rows={rows} />
+        </>
       )}
       </Container>
     )
