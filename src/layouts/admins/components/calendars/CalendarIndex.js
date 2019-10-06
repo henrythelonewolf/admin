7073ea9e50b36c705bb0451d48dc6a61c9727be1
alkyu92 <alkyu92@gmail.com>
2019-10-07 01:04:51 +0800

    wip

--- src/layouts/admins/components/calendars/CalendarIndex.js
+++ src/layouts/admins/components/calendars/CalendarIndex.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Grid, Card } from 'semantic-ui-react';
+import uuidv4 from 'uuid/v4';
 
 import {
   Calendar,
@@ -14,17 +15,64 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
 
 const localizer = momentLocalizer(moment);
-const DnDCalendar = withDragAndDrop(Calendar)
+const DnDCalendar = withDragAndDrop(Calendar);
 
 export default class CalendarIndex extends React.Component {
   state = {
     events: [
       {
+        id: uuidv4(),
         start: new Date(),
-        end: new Date(moment()),
-        title: "Some title"
-      }
-    ]
+        end: new Date(),
+        title: "First title",
+        isAllDay: false,
+      },
+      {
+        id: uuidv4(),
+        start: new Date(),
+        end: new Date(),
+        title: "Second title",
+        isAllDay: true,
+      },
+    ],
+    draggedEvent: null,
+  }
+
+  handleDragStart = event => {
+    this.setState({ draggedEvent: event })
+  }
+
+  dragFromOutsideItem = () => {
+    const { draggedEvent } = this.state;
+    return draggedEvent;
+  }
+
+  onDropFromOutside = ({ start, end, allDay }) => {
+    const { draggedEvent } = this.state;
+    const event = {
+      title: "DnD Title",
+      start,
+      end,
+      isAllDay: allDay,
+    }
+    this.setState({ draggedEvent: null });
+    this.newEvent(event);
+  }
+
+  newEvent(event){
+    const { events } = this.state;
+
+    let newEvt = {
+      id: uuidv4(),
+      title: event.title,
+      start: event.start,
+      end: event.end,
+      isAllDay: event.isAllDay,
+    }
+
+    this.setState({
+      events: events.concat([newEvt])
+    })
   }
 
   onEventDrop = ({ event, start, end, isAllDay: droppedOnAllDaySlot }) => {
@@ -47,39 +95,89 @@ export default class CalendarIndex extends React.Component {
     this.setState({ events: nextEvents });
   };
 
-  onEventResize = ({ event, start, end }) => {
-    const { events } = this.state;
-
-    const nextEvents = events.map(existingEvent => {
-      return existingEvent.id === event.id ? { ...existingEvent, start, end } : existingEvent
-    })
-
-    this.setState({ events: nextEvents });
+  onSelectSlot = (event) => {
+    alert('Slot selected: ' + event.start)
+    console.log(event.start);
   }
 
-  newEvent = (event) => {
-
+  onDoubleClickEvent = (event) => {
+    alert('Double clicked: '
+    + 'ID: ' + event.id
+    + ' Title: ' + event.title
+    + ' Start at: ' + event.start
+    + ' End at: ' + event.end
+    + ' isAllDay?: ' + event.isAllDay
+    )
   }
 
   render(){
     const { events } = this.state;
 
     return (
-      <Container>
-        <DnDCalendar
-          selectable
-          resizable
-          localizer={localizer}
-          events={events}
-          onEventDrop={this.onEventDrop}
-          onEventResize={this.onEventResize}
-          onSelectSlot={this.newEvent}
-          onDragStart={console.log}
-          defaultView={Views.MONTH}
-          defaultDate={new Date()}
-          style={{ height: '70vh'}}
-        />
+      <Container fluid style={styles.container}>
+        <Grid columns={2}>
+          <Grid.Row>
+            <Grid.Column>
+            <div>Agenda</div>
+            </Grid.Column>
+            <Grid.Column>
+            <Card className="examples--header" style={{ display: 'flex' }}>
+        <div
+          style={{
+            display: 'flex',
+            flex: 1,
+            justifyContent: 'center',
+            flexWrap: 'wrap',
+          }}
+        >
+          <h4 style={{ color: 'gray', width: '100%' }}>
+            Outside Drag Sources
+          </h4>
+            <div
+              style={{
+                border: '2px solid gray',
+                borderRadius: '4px',
+                width: '100px',
+                margin: '10px',
+              }}
+              draggable="true"
+              key={'id'}
+              onDragStart={() =>
+                this.handleDragStart({ title: 'DnD Outside' })
+              }
+            >
+              Test DnD Outside
+            </div>
+
+        </div>
+
+      </Card>
+              <DnDCalendar
+                selectable
+                localizer={localizer}
+                events={events}
+                onEventDrop={this.onEventDrop}
+                dragFromOutsideItem={this.dragFromOutsideItem}
+                onDropFromOutside={this.onDropFromOutside}
+                onSelectSlot={this.newEvent}
+                onDoubleClickEvent={this.onDoubleClickEvent}
+                onDragStart={console.log}
+                defaultView={Views.MONTH}
+                defaultDate={new Date()}
+                style={{ height: '71vh'}}
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
       </Container>
     )
   }
+}
+
+const styles = {
+  container: {
+    paddingRight: 50,
+    paddingLeft: 50,
+  },
 }
--- src/layouts/admins/components/orders/OrdersIndex.js
+++ src/layouts/admins/components/orders/OrdersIndex.js
@@ -1,88 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container, Tab, Button } from 'semantic-ui-react';
-
-import ReactDataGrid from 'react-data-grid';
-import { Toolbar, Data, Filters } from 'react-data-grid-addons';
 
 import PendingOrders from './PendingOrders';
 import ProcessedOrders from './ProcessedOrders';
 import OutForDeliveryOrders from './OutForDeliveryOrders';
 import OnHoldOrders from './OnHoldOrders';
 
-const defaultColumnProperties = {
-  filterable: true,
-  width: 160,
-}
-
-const selectors = Data.Selectors;
-
-const {
-  NumericFilter,
-  AutoCompleteFilter,
-  MultiSelectFilter,
-  SingleSelectFilter,
-} = Filters;
-
-const rows = [
-  { id: 1, title: 'First Row', description: 'This is first row' },
-  { id: 2, title: 'Second Row', description: 'This is second row' }
-]
-
-const columns = [
-  {
-    key: 'id',
-    name: 'ID',
-    filterRenderer: NumericFilter
-  },
-  {
-    key: 'title',
-    name: 'Title',
-    filterRenderer: MultiSelectFilter,
-  },
-  {
-    key: 'description',
-    name: 'Description',
-  },
-].map( col => ({ ...col, ...defaultColumnProperties }));
-
-const handleFilterChange = filter => filters => {
-  const newFilters = { ...filters };
-
-  if (filter.filterTerm) {
-    newFilters[filter.column.key] = filter;
-  } else {
-    delete newFilters[filter.column.key];
-  }
-
-  return newFilters;
-}
-
-function getValidFilterValues(rows, columnId){
-  return rows.map( row => row[columnId]).filter( (item, i, a) => {
-    return i === a.indexOf(item);
-  })
-}
-
-function getRows(rows, filters){
-  return selectors.getRows({ rows, filters });
-}
-
-function MyDataGrid({ rows }) {
-  const [filters, setFilters] = useState({});
-  const filteredRows = getRows(rows, filters);
-  return (
-    <ReactDataGrid
-      rowGetter={ i => filteredRows[i] }
-      columns={columns}
-      rowsCount={filteredRows.length}
-      minHeight={500}
-      toolbar={<Toolbar enableFilter={true} />}
-      onAddFilter={ filter => setFilters(handleFilterChange(filter))}
-      onClearFilters={ () => setFilters({})}
-      getValidFilterValues={ columnKey => getValidFilterValues(rows, columnKey)}
-    />
-  )
-}
+import ListsIndex from './../lists/ListsIndex';
 
 export default class BoilerPlate extends React.Component {
   mainPanes = [
@@ -115,7 +39,7 @@ export default class BoilerPlate extends React.Component {
         <Button onClick={ () => this.setState({ toggleView: !toggleView})}>
         Individual View
         </Button>
-        <MyDataGrid rows={rows} />
+        <ListsIndex />
         </>
       )}
       </Container>
